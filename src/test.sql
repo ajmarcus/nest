2b7300c2495dcf0f626fcd1cde7e8a0b9d622c51
ajmarcus <little@merman>
2013-01-15 01:50:25 -0500

    added test script

--- src/test.sql
+++ src/test.sql
@@ -2,12 +2,14 @@ wc -l yelp.json
 474434
 
 select count(1) from raw_nest;
-
-_c0
 474434
 
 
 select count(1) from fact_nest;
-
-_c0
 474434
+
+select type, count(1) from fact_nest group by type;
+business  13490
+review  330071
+user  130873
+
