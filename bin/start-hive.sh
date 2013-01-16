65f9a94ada5a60f15c04a815766c2662cdb34d09
ajmarcus <little@merman>
2013-01-15 21:11:51 -0500

    added lappy_emr

--- bin/start-hive.sh
+++ bin/start-hive.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-lappy_emr --create --alive --hive-interactive \
+lappy_emr='/home/openbi/emr/elastic-mapreduce-cli/elastic-mapreduce -c /home/openbi/.emr/lappy.json'
+
+$lappy_emr --create --alive --hive-interactive \
   --name 'hive nest' \
   --instance-group master --instance-count 1 \
   --instance-type m2.xlarge \
